b4ea24004f83d8dec9d519ce2f1904d3fbacfdbb
Greg Stamper <gastamper@gmail.com>
2019-09-20 12:23:07 -0400

    Updates for 9.20

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ extern crate regex;
 use regex::Regex;
 extern crate clap;
 use clap::{Arg, App};
+#[macro_use] extern crate log;
+extern crate env_logger;
+use env_logger::Env;
 
 fn exitout(arg: String) {
-    println!("{}", arg);
+    error!("{}", arg);
     std::process::exit(1);
 }
 
@@ -54,25 +57,49 @@ fn main() -> Result<(), Error> {
                     .long("url")
                     .help("URL of specific mailing list entry to parse")
                     .takes_value(true))
+                    .arg(Arg::with_name("verbose")
+                    .short("v")
+                    .long("verbose")
+                    .help("verbosity level")
+                    .multiple(true))
                     .get_matches();
+    let verbosity = match matches.occurrences_of("verbose") {
+        0 => "info",
+        1 => "debug",
+        2 | _ => "trace",
+    };
+    // Turn off logging from other packages
+    let baseloglevel = ",tokio=info,hyper=info,tokio_reactor=info,reqwest=info,want=info,mio=info";
+    env_logger::from_env(Env::default().default_filter_or(format!("{},{}", verbosity, baseloglevel))).init();
+    
     let request_url = matches.value_of("url").unwrap_or("");
     if request_url == "" {
-        exitout(String::from("No URL"));
+        exitout(String::from("No URL specified"));
         }
 //	let request_url = format!("https://lists.centos.org/pipermail/centos-cr-announce/2019-September/006197.html");
-	println!("URL: {}", request_url);
+	info!("URL: {}", request_url);
 	//let mut response = reqwest::get(&request_url.to_string())?;
 	let mut response = handler(&request_url.to_string()); 
 	
     
     // Check if HTTP request worked; return status code and URL if failure
 	if ! response.status().is_success() {
-		//println!("Error {} for {}", response.status(), request_url);
 		exitout(format!("Error {} for {}", response.status(), request_url));
 	}
-    println!("Got here");
 	let out = response.text()?;
 
+    // Valid list entries take the format:
+    // [...]\n
+    // syncing to the mirrors: (sha256sum Filename)\n
+    // \n
+    // [arch (x86_64, Source, etc)]\n
+    // [sha256sum] [Filename]\n
+    //
+    // [...]\n
+    // \n
+    //
+    // Split entries by two newlines in succession and then check if the current section
+    // is applicable to the architecture supplied.
 	let split = out.split("\n\n");
 	let mut data = HashSet::new();;
 	
@@ -80,7 +107,7 @@ fn main() -> Result<(), Error> {
 		let mut matched = false;
 		for line in s.split("\n") {
 			if matched == true { 
-				println!("Inserting: {}", line);
+				trace!("Inserting: {}", line);
 				let hashsplit = line.split("  ");
 				for item in hashsplit {
 					if item.ends_with(".rpm") {
@@ -90,7 +117,7 @@ fn main() -> Result<(), Error> {
 			}
 			// Find x86_64 heading and set flag to begin inserting lines into set
 			if line == "x86_64:" { 
-				println!("Found x86_64");
+				debug!("Found x86_64");
 				matched = true;
 			}
 		}
@@ -98,14 +125,20 @@ fn main() -> Result<(), Error> {
 	let mut newset = HashSet::new();
 	// Matches only package name assuming no nonconventional naming
 	let re = Regex::new(r"(.*)(?:(-[^-]*-[^-]*$))").unwrap();
-	println!("{:#?}", data);
+	trace!("{:#?}", data);
 	for item in data {
 		let packagename = re.captures(item).unwrap();
-		println!("Got package: {}", packagename.get(1).map_or("", |m| m.as_str()));
+		debug!("Got package: {}", packagename.get(1).map_or("", |m| m.as_str()));
 		newset.insert(packagename.get(1).map_or("", |m| m.as_str()));
 	}
 	let finalstr = newset.into_iter().collect::<Vec<&str>>().join(" ");
-	println!("Final package list: {}", finalstr);
-	println!("yum-config-mgr --enablerepo=cr; yum update {}; yum-config-mgr --disablerepo=cr", finalstr);
+	debug!("Final package list: {}", finalstr);
+
+//  Logic for prepending/appending CR repository enabling
+//  if opt.crrepo == true { info!("yum-config-mgr --enablerepo=cr; yum update
+//  {};  yum-config-mgr --disablerepo=cr", finalstr); }
+//  else { info!("yum update {}", finalstr); }
+//  elif opt.pkgonly == true { info!("{}"); }
+	info!("yum-config-mgr --enablerepo=cr; yum update {}; yum-config-mgr --disablerepo=cr", finalstr);
 	Ok(())
 }
